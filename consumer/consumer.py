a09e23b41ae173ed288f3b1ea74323e7617ae209
Frederic Morin <frederic.morin@ara-uas.com>
2023-09-15 12:47:41 -0400

    Add support for custom endpoint url to consumer.py

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -8,26 +8,33 @@ import boto3
 import requests
 
 JENKINS_URL = os.environ['JENKINS_URL']
-SQS_QUEUE_URL = os.environ['SQS_QUEUE_URL']
+SQS_ENDPOINT_URL = os.environ.get('SQS_ENDPOINT_URL')
+SQS_QUEUE = os.environ['SQS_QUEUE']
 SQS_REGION = os.environ['SQS_REGION']
 
-sqs = boto3.client('sqs', region_name=SQS_REGION)
+if SQS_ENDPOINT_URL:
+    sqs = boto3.resource('sqs',
+                         endpoint_url=SQS_ENDPOINT_URL,
+                         region_name=SQS_REGION,
+                         use_ssl=False)
+else:
+    sqs = boto3.resource('sqs', region_name=SQS_REGION)
 
 print("Webhook consumer starting up!")
 
 while True:
-    message_list = sqs.receive_message(
-        QueueUrl=SQS_QUEUE_URL,
-        WaitTimeSeconds=20,
+    queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE)
+    message_list = queue.receive_messages(
+        WaitTimeSeconds=5,
         MaxNumberOfMessages=10
     )
-    if not message_list or "Messages" not in message_list:
+    if not message_list:
         time.sleep(5)
         continue
 
-    for message in message_list["Messages"]:
-        print(f"Got message: {message['MessageId']} at {datetime.datetime.now().isoformat()}")
-        parsed = json.loads(message["Body"])
+    for message in message_list:
+        print(f"Got message: {message.message_id} at {datetime.datetime.now().isoformat()}")
+        parsed = json.loads(message.body)
         original_headers = parsed['headers']
         payload = parsed['payload']
 
@@ -46,9 +53,9 @@ while True:
         print(resp.text)
 
         # Delete the message if we made it this far.
-        sqs.delete_message(
-            QueueUrl=SQS_QUEUE_URL,
-            ReceiptHandle=message['ReceiptHandle']
-        )
+        queue.delete_messages(Entries=[dict(
+            Id=message.message_id,
+            ReceiptHandle=message.receipt_handle
+        )])
 
     time.sleep(5)
